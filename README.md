49f423de2e29c337a7b71a78fbd2520ecd05cbca
Hunt3r0x <106396603+Hunt3r0x@users.noreply.github.com>
2024-05-13 21:53:58 +0300

    FUCK ISRAEL

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
 # File Monitor Tool
 
-The `eyer.sh` script is a bash script that monitors a file for modifications and sends a notification when the file is modified. The script takes three optional parameters:
+The `eyer.sh` script is a bash script that monitors a file for modifications and sends a notification when the file is modified. The script takes several optional parameters:
 
-- `-f`: The path to the file to monitor
+- `-f`: The path to the file(s) to monitor
 - `-i`: The interval (in seconds) between checks for modifications (default: 5)
 - `-n`: The ID of the [notify](https://github.com/projectdiscovery/notify) tool service to use
+- `-up`: Specify the action to send the file when a file is updated
+- `-filename`: Specify the name of the file
 
 ## Usage
 
-To use the script, simply provide the path to the file to monitor using the `-f` flag, and the ID of the [notify](https://github.com/projectdiscovery/notify) to use using the `-n` flag. You can also optionally specify the interval between checks for modifications using the `-i` flag.
+To use the script, simply provide the path to the file(s) to monitor using the `-f` flag, and the ID of the [notify](https://github.com/projectdiscovery/notify) service to use using the `-n` flag. You can also optionally specify the interval between checks for modifications using the `-i` flag, the action to perform when a file is updated using the `-up` flag, and the name of the file using the `-filename` flag.
 
 #### To monitor one file
 
@@ -16,8 +18,20 @@ To use the script, simply provide the path to the file to monitor using the `-f`
 ./eyer.sh -f /path/to/file -n notify-id -i 10
 ```
 
-#### To monitor multible files
+#### To monitor multiple files
 
 ```bash
 ./eyer.sh -f /path/to/file1 -f /path/to/file2 -f /path/to/file3 -n notify-id -i 10
+```
+
+#### To monitor files and send updated file to discord
+
+```bash
+./eyer.sh -f /path/to/file1 -f /path/to/file2 -f /path/to/file3 -n notify-id -i 10 -up -filename "subdomains-of-hackerone"
+```
+
+For more details on usage and available options, you can run the script with the `-h` or `--help` flag.
+
+```bash
+./eyer.sh --help
 ```
--- eyer.sh
+++ eyer.sh
@@ -9,7 +9,8 @@ YELLOW='\033[0;33m'
 BLUE='\033[0;34m'
 NC='\033[0m'
 
-banner=$(cat << "EOF"
+banner=$(
+    cat <<"EOF"
  
    ▄████████ ▄██   ▄      ▄████████    ▄████████    ▄████████ 
   ███    ███ ███   ██▄   ███    ███   ███    ███   ███    ███ 
@@ -20,28 +21,70 @@ banner=$(cat << "EOF"
   ███    ███ ███   ███   ███    ███   ███    ███   ███    ███ 
   ██████████  ▀█████▀    ██████████   ██████████   ███    ███ 
                                                    ███    ███     
-                            by H1NTR0X01 @71ntr 
+                            by H1NTR0X1 @71ntr 
                             
 EOF
 )
 
 echo -e "${BLUE}$banner${NC}"
 
-FILES_TO_MONITOR=()
+display_usage() {
+    echo -e "${YELLOW}Usage:${NC} $0 ${GREEN}[-f <file1> [<file2> ...]] [-i <interval>] [-n <notification_id>] [-up <action>] [-filename <file_name>] [-h|--help]${NC}\n"
+    echo -e "${YELLOW}Options:${NC}"
+    echo -e "  ${GREEN}-f <file1> [<file2> ...]:${NC} Specify file(s) to monitor"
+    echo -e "  ${GREEN}-i <interval>:${NC} Specify the check interval in seconds (default: 5)"
+    echo -e "  ${GREEN}-n <notification_id>:${NC} Specify the notification ID"
+    echo -e "  ${GREEN}-up <action>:${NC} Specify the action to send the file when a file is updated"
+    echo -e "  ${GREEN}-filename <file_name>:${NC} Specify the name of the file"
+    echo -e "  ${GREEN}-h, --help:${NC} Display this help message and exit"
+    echo -e ""
+}
 
-while getopts ":f:i:n:" opt; do
-    case $opt in
-        f)
-            FILES_TO_MONITOR+=("$OPTARG")
-            ;;
-        i)
-            CHECK_INTERVAL=$OPTARG
-            ;;
-        n)
-            NOTIFICATION_ID=$OPTARG
-            ;;
+if [[ $# -eq 0 ]]; then
+    display_usage
+    exit 1
+fi
+
+FILES_TO_MONITOR=()
+UP_ACTION=""
+
+while [ -n "$1" ]; do
+    case $1 in
+    -f)
+        shift
+        FILES_TO_MONITOR+=("$1")
+        ;;
+    -i)
+        shift
+        CHECK_INTERVAL=$1
+        ;;
+    -n)
+        shift
+        NOTIFICATION_ID=$1
+        ;;
+    -up)
+        shift
+        UP_ACTION=$1
+        ;;
+    -filename)
+        shift
+        FILE_NAME=$1
+        ;;
     esac
+    shift
 done
+
+get_webhook_url() {
+    local id="$1"
+    local file="$HOME/.config/notify/provider-config.yaml"
+    local url=$(grep -A 4 "id: \"$id\"" "$file" | grep 'discord_webhook_url' | cut -d '"' -f 2)
+    if [ -z "$url" ]; then
+        echo "Error: ID '$id' not found or no webhook URL found for the ID '$id'." >&2
+        return 1
+    else
+        echo "$url"
+    fi
+}
 
 if [ ${#FILES_TO_MONITOR[@]} -eq 0 ]; then
     echo -e "${RED}Please specify file(s) to monitor with the -f flag.${NC}"
@@ -53,54 +96,63 @@ if [ -z "$NOTIFICATION_ID" ]; then
     exit 1
 fi
 
+notifyz() {
+    WEBHOOK_URL=$(get_webhook_url "$NOTIFICATION_ID")
+    message_up="
+### Hey, Bro!
+UPDATED SUBDOAMINS FILE FOR => **${FILE_NAME}**
+LAST MODIFIED => **${current_time}**
+"
+    response=$(curl -s -q -X POST \
+        -H "Content-Type: multipart/form-data" \
+        -F "file=@$file;filename=${FILE_NAME}.txt" \
+        -F "content=${message_up}" \
+        "$WEBHOOK_URL")
+}
+
+notify_user() {
+    local message="$1"
+    local notification_id="$2"
+    echo "$message" | notify -silent -id "$notification_id" >/dev/null 2>&1
+}
+
 monitor_file_changes() {
     local file="$1"
     local last_modified=$(date -r "$file" +%s)
     local deleted=false
-    
+
     while true; do
         if [ ! -f "$file" ]; then
             if [ "$deleted" = false ]; then
                 local current_time=$(date +"%Y-%m-%d %H:%M:%S")
                 local user=$(whoami)
-                
+
                 echo -e "${YELLOW}[$current_time] File $file has been deleted by $user!${NC}"
-                echo "[$current_time] $user deleted $file" >> "$LOG_FILE"
-                echo "- File $file has been deleted! at [$current_time] " | notify -id $NOTIFICATION_ID > /dev/null 2>&1
-                
+                echo "[$current_time] $user deleted $file" >>"$LOG_FILE"
+                notify_user "- File $file has been deleted! at [$current_time] " "$NOTIFICATION_ID"
                 deleted=true
             fi
         else
             deleted=false
             local current_modified=$(date -r "$file" +%s)
-            
+
             if [[ "$current_modified" != "$last_modified" ]]; then
                 local current_time=$(date +"%Y-%m-%d %H:%M:%S")
                 local user=$(whoami)
-                
+
                 echo -e "${GREEN}[$current_time] File $file has been modified by $user!${NC}"
-                echo "[$current_time] $user made changes in $file" >> "$LOG_FILE"
-                echo "- File $file has been modified! at [$current_time] " | notify -id $NOTIFICATION_ID > /dev/null 2>&1
-                
+                echo "[$current_time] $user made changes in $file" >>"$LOG_FILE"
+
+                if [ -n "$UP_ACTION" ]; then
+                    notifyz
+                fi
                 last_modified=$current_modified
             fi
         fi
-
         sleep "$CHECK_INTERVAL"
     done
 }
 
-# Function to handle exit signals
-exit_handler() {
-    pkill -P $$ # Terminate child processes
-    echo -e "${RED}Monitoring terminated.${NC}"
-    exit
-}
-
-# Register exit signals handler
-trap exit_handler EXIT SIGINT SIGTERM
-
-# Start monitoring for each file in the background
 for file in "${FILES_TO_MONITOR[@]}"; do
     if [ ! -f "$file" ]; then
         echo -e "${RED}File '$file' does not exist.${NC}"
@@ -110,5 +162,4 @@ for file in "${FILES_TO_MONITOR[@]}"; do
     monitor_file_changes "$file" &
 done
 
-# Wait for background processes to finish before exiting
 wait
